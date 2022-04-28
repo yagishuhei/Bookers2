c9d15642954c18963fd86c26a03fd077c88cc2cc
EC2 Default User <ec2-user@ip-172-31-21-224.ec2.internal>
2022-04-28 09:54:00 +0000

    [Fix]layout

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,6 @@ ActiveRecord::Schema.define(version: 2022_04_21_074128) do
     t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
